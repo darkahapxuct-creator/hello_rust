adb44502c943d5e877c5b382eb4807bae5620819
root <root@localhost.localdomain>
2026-06-18 10:51:21 +0000

    Added interactive name prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Тест компиляции успешно пройден!");
+    print!("Введите ваше имя: ");
+    io::stdout().flush().unwrap();
+
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).unwrap();
+
+    println!("Привет, {}! Добро пожаловать в Rust на Android!", name.trim());
 }
